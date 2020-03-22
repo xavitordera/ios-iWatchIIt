febb85f97f69603954b64a567fe0d625d17ab1d7
Xavi Tordera <xavier.tordera@slashmobility.com>
2020-03-22 17:09:27 +0100

    - Created Project and project dependencies

---
+++ iWatchIt/iWatchIt/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  iWatchIt
+//
+//  Created by Xavi Tordera on 22/03/2020.
+//  Copyright © 2020 Xavi Tordera. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
